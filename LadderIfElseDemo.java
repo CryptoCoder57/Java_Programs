493ec979351c95a7030b483339d91f131f658065
CryptoCoder57 <91009052+CryptoCoder57@users.noreply.github.com>
2021-09-22 21:42:11 +0530

    Created LadderIfElseDemo.java

---
+++ LadderIfElseDemo.java
@@ -0,0 +1,27 @@
+public class LadderIfElseDemo 
+{
+	public static void main(String args[])
+	{
+		int i = 4;	//Consider i is the choice
+		if(i==1)
+		{
+			System.out.println("Choice is One");
+		}
+		else if(i==2)
+		{
+			System.out.println("Choice is Two");
+		}
+		else if(i==3)
+		{
+			System.out.println("Choice is Three");
+		}
+		else if(i==4)
+		{
+			System.out.println("Choice is Four");
+		}
+		else 	// this is a optional statement
+		{
+			System.out.println("Choice is Invalid");
+		}
+	}
+}
