eee4e0069bce5b1251658897e03d3e4764c2b98a
CryptoCoder57 <91009052+CryptoCoder57@users.noreply.github.com>
2021-09-26 10:52:09 +0530

    Created JavaFundamentalProgram6.java

---
+++ JFP6.java
@@ -0,0 +1,12 @@
+// Program to write an infinite loop using do while condition.
+public class JFP6 
+{
+	public static void main(String args[])
+	{
+		do
+		{
+			System.out.println("This is an infinite loop");
+      
+		}while(true);
+	}
+}
