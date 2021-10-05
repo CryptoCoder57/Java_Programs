51ee94be0166b3bb519f42d313ecc97d72f11973
CryptoCoder57 <91009052+CryptoCoder57@users.noreply.github.com>
2021-10-06 04:24:20 +0530

    Created AlphabetsDisplayDemo.java

---
+++ AlphabetsDisplayDemo.java
@@ -0,0 +1,18 @@
+public class AlphabetsDisplayDemo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Alphabets in lower case are : ");
+		for(char c='a';c<='z';c++)
+		{
+			System.out.print(c+" ");
+		}
+		System.out.println();
+		System.out.println("======================");
+		System.out.println("Alphabets in upper case are : ");
+		for(char c='A';c<='Z';c++)
+		{
+			System.out.print(c+" ");
+		}
+	}
+}
